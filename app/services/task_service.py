81858eb70356aa6b98f27e999ed01254aab33b1c
leressa-b <lnb2000@hw.ac.uk>
2025-07-18 02:36:16 +0400

    updated task_service

--- app/services/task_service.py
+++ app/services/task_service.py
@@ -1,5 +1,31 @@
 from typing import List, Optional
 from app.models.task import Task, TaskFilter
+import time
+
+class SmartCache:
+    def __init__(self, max_size=100):
+        self._cache = {}
+        self._access_times = {}
+        self._max_size = max_size
+    
+    def get(self, key):
+        if key in self._cache:
+            self._access_times[key] = time.time()
+            return self._cache[key]
+        return None
+    
+    def put(self, key, value):
+        current_time = time.time()
+        
+        if len(self._cache) >= self._max_size:
+            oldest_key = min(self._access_times.keys(), 
+                           key=lambda k: self._access_times[k])
+            del self._cache[oldest_key]
+            del self._access_times[oldest_key]
+        
+        self._cache[key] = value
+        self._access_times[key] = current_time
+
 
 class TaskService:
     def __init__(self, db):
